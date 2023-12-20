90a2799bd1342562634c1e56fff8734e3000665a
PiedraAndrick <65957499+PiedraAndrick@users.noreply.github.com>
2023-12-19 23:05:51 -0500

    Servicio

---
+++ src/main/java/com/example/proyectou12023/Controlador/Controlador.java
@@ -0,0 +1,5 @@
+package com.example.proyectou12023.Controlador;
+
+public class Controlador {
+
+}
--- src/main/java/com/example/proyectou12023/Modelo/Equipo.java
+++ src/main/java/com/example/proyectou12023/Modelo/Equipo.java
@@ -1,10 +1,15 @@
 package com.example.proyectou12023.Modelo;
 
+import lombok.Data;
+
+@Data
 public class Equipo {
     private static int contadorId = 1;
 
     private Integer IdEquipo;
-    private Integer IdPersona;
+    private String IdPersona;
+    private String nombre;
+
     public Equipo() {
         this.IdEquipo = contadorId++;
     }
