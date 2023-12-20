69fbfb43489279640722d45e58dec27082492a28
Jorge Borrero <jorgerey2011@gmail.com>
2023-12-19 23:24:14 -0500

    Add files via upload
    Controlador

--- src/main/java/com/example/proyectou12023/Modelo/Institucion.java
+++ src/main/java/com/example/proyectou12023/Modelo/Institucion.java
@@ -1,19 +1,5 @@
 package com.example.proyectou12023.Modelo;
 
-import lombok.Data;
-
-import java.util.Date;
-@Data
 public class Institucion {
-    private static int contadorId = 1;
-
-    private Integer IdInstitucion;
-    private String Nombre;
-    private String Direccion;
-    private String Telefono;
-    private String Email;
-    public Institucion() {
-        this.IdInstitucion = contadorId++;
-    }
 
 }
---
+++ src/main/java/com/example/proyectou12023/Repositorio/Repositorio.java
@@ -0,0 +1,5 @@
+package com.example.proyectou12023.Repositorio;
+
+public class Repositorio {
+
+}
