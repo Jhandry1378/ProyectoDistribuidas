e8240524cfecd8424bfac1a85b7c73fa37c1b6d9
PiedraAndrick <65957499+PiedraAndrick@users.noreply.github.com>
2023-12-20 00:06:12 -0500

    Importaciones Servicios.java

--- src/main/java/com/example/proyectou12023/Servicios/Servicios.java
+++ src/main/java/com/example/proyectou12023/Servicios/Servicios.java
@@ -1,5 +1,11 @@
+package com.example.proyectou12023.Servicios;
 
+import com.example.proyectou12023.Modelo.*;
+import com.example.proyectou12023.Repositorio.Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 @Service
 public class Servicios {
 
